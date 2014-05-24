9fe12670c6b78d7740d93f0e26fc09361c645ba6
Yan Gorshtenin <support@netlodge.net>
2014-05-23 17:22:35 -0700

    Commit #1

---
+++ ActivityTrackingViewController.h
@@ -0,0 +1,13 @@
+//
+// ActivityTrackingViewController.h
+// iOS7Sampler
+//
+// Created by PvmHD on 5/23/14.
+// Copyright (c) 2014 Yan Gorshtenin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityTrackingViewController : UIViewController
+
+@end
